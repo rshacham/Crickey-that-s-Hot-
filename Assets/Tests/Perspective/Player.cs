3d0e8ab4a2bc0acd31dad0f39089b65782f88239
erankramf <eran.kramf@mail.huji.ac.il>
2022-03-16 08:55:10 +0200

    Test 1 - perspective turning
    Started testing mechanics we arent sure of.

---
+++ Assets/Tests/Perspective/Player.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public GameObject player;
+    public Rigidbody2D rigid;
+    public GameObject world;
+
+    public float speed;
+    public int rotationSpeed;
+
+    private int rotate = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rotate = 0;
+
+        // rigid.velocity = Vector2.right*speed;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (rotate == 0)
+        {
+            Vector2 loc = rigid.position;
+            loc.x += Time.deltaTime * speed;
+            rigid.position = loc;
+        }
+        if (Input.GetKeyUp(KeyCode.Q))
+        {
+            // world.transform.Rotate(new Vector3(0,0,1),-90);
+            rotate = 18*rotationSpeed;
+        }
+
+        if (rotate >0)
+        {
+            world.transform.Rotate(new Vector3(0,0,1),(-5f / rotationSpeed));
+            rotate--;
+        }
+    }
+}
